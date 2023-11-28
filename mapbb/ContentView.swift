9c70d6a31b5ecd41d78fa5d1df259b2616a31d63
haruto <32rurio@gmail.com>
2023-11-29 03:19:19 +0900

    Initial Commit

---
+++ mapbb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mapbb
+//
+//  Created by CHiTTii iPHONE on 2023/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mapbb/mapbbApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapbbApp.swift
+//  mapbb
+//
+//  Created by CHiTTii iPHONE on 2023/11/29.
+//
+
+import SwiftUI
+
+@main
+struct mapbbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
