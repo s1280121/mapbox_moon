e136e7a4068f3118c0a7e9c4f6da4ebde865e8d4
haruto <32rurio@gmail.com>
2023-12-11 06:17:00 +0900

    Third commit

--- mapbb/ContentView.swift
+++ mapbb/ContentView.swift
@@ -1,68 +1,3 @@
-//import SwiftUI
-//import MapboxMaps
-//
-//struct ContentView: View {
-//    var body: some View {
-//        MapBoxMapView()
-//    }
-//}
-//
-//struct MapBoxMapView: UIViewControllerRepresentable {
-//    func makeUIViewController(context: Context) -> MapViewController {
-//        return MapViewController()
-//    }
-//
-//    func updateUIViewController(_ uiViewController: MapViewController, context: Context) {
-//    }
-//}
-//
-//class MapViewController: UIViewController {
-//    internal var mapView: MapView!
-//    private var coordinatesLabel: UILabel!
-//
-//    override public func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        let myResourceOptions = ResourceOptions(accessToken: "accessToken")
-//        let cameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), zoom: 0.1)
-//
-//        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: cameraOptions, styleURI: StyleURI(rawValue: "mapbox://styles/araharuto/clpika41t007l01olast88bzu"))
-//
-//        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
-//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//
-//        self.view.addSubview(mapView)
-//
-//        // Add a label to display coordinates
-//        coordinatesLabel = UILabel(frame: CGRect(x: 16, y: 25, width: 400, height: 60))
-//        coordinatesLabel.textColor = .white
-//        coordinatesLabel.backgroundColor = .black
-//        coordinatesLabel.alpha = 0.7
-//        coordinatesLabel.layer.cornerRadius = 5
-//        coordinatesLabel.clipsToBounds = true
-//
-//        self.view.addSubview(coordinatesLabel)
-//
-//        // Set up tap gesture recognizer
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(handleMapTap(sender:)))
-//        mapView.addGestureRecognizer(tap)
-//
-//        mapView.mapboxMap.onNext(.mapLoaded) { _ in
-//            // Handle map loaded event if needed
-//        }
-//    }
-//
-//    @objc private func handleMapTap(sender: UITapGestureRecognizer) {
-//        // タップした地図上の点の座標を取得
-//        let point = sender.location(in: mapView)
-//        let coordinates = mapView.mapboxMap.coordinate(for: point)
-//        // 座標を表示
-//        coordinatesLabel.text = "Lat: \(coordinates.latitude), Lon: \(coordinates.longitude)"
-//    }
-//}
-//#Preview {
-//    ContentView()
-//}
 import SwiftUI
 import MapboxMaps
 
@@ -72,6 +7,8 @@ class MapViewModel: ObservableObject {
 
 struct ContentView: View {
     @StateObject private var mapViewModel = MapViewModel()
+//    @State var showingPopUp = false
+
 
     var body: some View {
         VStack {
@@ -82,10 +19,33 @@ struct ContentView: View {
                 Text("Tapped Coordinates: \(tappedCoordinates.latitude), \(tappedCoordinates.longitude)")
                     .foregroundColor(.black)
                     .padding()
+//                Button(action: {
+//                    withAnimation {
+//                        showingPopUp = true
+//                    } //trueにしないと画面遷移されない
+//                }, label: {
+//                    Text("View Spectra")
+//                    .padding()
+//                    .background(Color.white)
+//                    .cornerRadius(12)
+//                })
+//                .fullScreenCover(isPresented: $showingPopUp) { //フルスクリーンの画面遷移
+//                    PopupView(isPresent: $showingPopUp, info: url, Lat: tappedCoordinates.latitude, Lon: tappedCoordinates.longitude)
+//            }
+
             }
         }
     }
 }
+//
+//struct PopupView: View {
+//    var info:String
+//    @State var coordinate:[String]
+//    
+//    var body: some View {
+//        VStack {}
+//    }
+//}
 
 struct MapBoxMapView: View {
     @ObservedObject var mapViewModel: MapViewModel
@@ -132,25 +92,26 @@ class MapViewController: UIViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-                let myResourceOptions = ResourceOptions(accessToken: "accessToken")
-                let cameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), zoom: 0.1)
+        let myResourceOptions = ResourceOptions(accessToken: "accessToken")
+        let cameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), zoom: 0.1)
         
-                let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: cameraOptions, styleURI: StyleURI(rawValue: "mapbox://styles/araharuto/clpika41t007l01olast88bzu"))
+        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: cameraOptions, styleURI: StyleURI(rawValue: "mapbox://styles/araharuto/clpvzh143007x01rd13px558d"))
+
         
-                mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
-                mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
-                self.view.addSubview(mapView)
+        self.view.addSubview(mapView)
         
-                // Add a label to display coordinates
-                coordinatesLabel = UILabel(frame: CGRect(x: 16, y: 25, width: 400, height: 60))
-                coordinatesLabel.textColor = .white
-                coordinatesLabel.backgroundColor = .black
-                coordinatesLabel.alpha = 0.7
-                coordinatesLabel.layer.cornerRadius = 5
-                coordinatesLabel.clipsToBounds = true
+        // Add a label to display coordinates
+        coordinatesLabel = UILabel(frame: CGRect(x: 16, y: 25, width: 400, height: 60))
+        coordinatesLabel.textColor = .white
+        coordinatesLabel.backgroundColor = .black
+        coordinatesLabel.alpha = 0.7
+        coordinatesLabel.layer.cornerRadius = 5
+        coordinatesLabel.clipsToBounds = true
         
-                self.view.addSubview(coordinatesLabel)
+        self.view.addSubview(coordinatesLabel)
         
         // Set up tap gesture recognizer
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleMapTap(sender:)))
@@ -177,3 +138,4 @@ class MapViewController: UIViewController {
 #Preview {
     ContentView()
 }
+
